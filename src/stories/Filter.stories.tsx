e2a6a566cd18c4b967dc59da68d71df339ca2826
Johni Bahous <jb@digitaldavid.io>
2023-10-31 13:41:30 +0100

    Filter Component

---
+++ src/stories/Filter.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta } from "@storybook/react";
+import  Filter from "../components/Filter";
+
+
+export default {
+  title: "Filter",
+  component: Filter,
+} as Meta<typeof Filter>;
+
+
+export const Default = {
+
+};
